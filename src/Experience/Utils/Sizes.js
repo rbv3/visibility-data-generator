9e5c08903b2616f91ba69b80f45a0a164851192a
Rodrigo Valenca <rvalenca@amazon.com>
2024-01-21 23:17:45 -0300

    canvas size always 1024x1024

--- src/Experience/Utils/Sizes.js
+++ src/Experience/Utils/Sizes.js
@@ -3,16 +3,18 @@ export default class Sizes extends EventEmitter {
     constructor() {
         super()
 
-        this.width = window.innerWidth
-        this.height = window.innerHeight
-        this.pixelRatio = Math.min(window.devicePixelRatio, 2)
+        // this.width = window.innerWidth
+        // this.height = window.innerHeight
+        this.width = 1024
+        this.height = 1024
+        this.pixelRatio = 1
         
-        window.addEventListener('resize', () => {
-            this.width = window.innerWidth
-            this.height = window.innerHeight
-            this.pixelRatio = Math.min(window.devicePixelRatio, 2)
+        // window.addEventListener('resize', () => {
+        // this.width = window.innerWidth
+        // this.height = window.innerHeight
+        //     this.pixelRatio = Math.min(window.devicePixelRatio, 2)
 
-            this.trigger('resize')
-        })
+        //     this.trigger('resize')
+        // })
     }
 }

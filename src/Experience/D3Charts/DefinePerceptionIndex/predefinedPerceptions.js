ef00b66795c70f66f0c1cba1053d05dcfdaec0dd
Stefan Cobeli <stefancobeli@gmail.com>
2024-11-14 15:31:17 -0600

    Added predefined perception library.

---
+++ src/Experience/D3Charts/DefinePerceptionIndex/predefinedPerceptions.js
@@ -0,0 +1,32 @@
+predefinedFormulaLibrary = [{
+    "greeness": {
+        "name": "greeness",
+        "expression": "tree",
+        "semanticIdExpression": "5"
+    },
+    "openness": {
+        "name": "openness",
+        "expression": "sky",
+        "semanticIdExpression": "6"
+    },
+    "imageability": {
+        "name": "imageability",
+        "expression": "building + miscellaneous",
+        "semanticIdExpression": "0 + 7"
+    },
+    "enclosure": {
+        "name": "enclosure",
+        "expression": "( building + tree ) / ( road + surface + sidewalk + miscellaneous + building + tree )",
+        "semanticIdExpression": "( 0 + 5 ) / ( 2 + 4 + 3 + 7 + 0 + 5 )"
+    },
+    "walkability": {
+        "name": "walkability",
+        "expression": "sidewalk / ( road + sidewalk )",
+        "semanticIdExpression": "3 / ( 2 + 3 )"
+    },
+    "serenity": {
+        "name": "serenity",
+        "expression": "water + sky",
+        "semanticIdExpression": "1 + 6"
+    }
+}]
